83a377cde6bd04fa754f37724e0b6496eaf0f7a0
jenniferstarkey <jenniferrstarkey@yahoo.com>
2021-01-04 11:34:24 -0600

    user can see list of all current tags

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using TabloidMVC.Models;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+
+        public TagRepository(IConfiguration config) : base(config) { }
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT id, name FROM Tag";
+                    var tags = new List<Tag>();
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Tag tag = new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+                        tags.Add(tag);
+                    }
+
+                    reader.Close();
+                        return tags;
+
+                }
+            }
+        }
+
+    }
+}
